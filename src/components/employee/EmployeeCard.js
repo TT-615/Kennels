1f40e2f924d0d291b9c94d8708ad2b0cdbbb2df3
Tia Thomason <88596394+TT-615@users.noreply.github.com>
2021-09-28 23:12:17 -0500

    Who Let The Dogs Out? I Am.

---
+++ src/components/employee/EmployeeCard.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Employee.css"
+
+export const EmployeeCard = () => (
+    <section className="employee">
+        <h3 className="employee__name">Paul Hogan</h3>
+        <div className="employee__address">1001 Elm Street</div>
+    </section>
+)
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import { Kennel } from "./components/Kennel"
+import "./index.css"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Kennel />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
